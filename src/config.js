01d6526ff600a2d52e07bb157c10315b20cf5c43
Taha <mt8915022@gmail.com>
2026-06-12 02:39:14 +0500

    Update API_URL to use production backend URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
+// config.js or constants.js
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://fyp-backend-lgsp.onrender.com";
 export default API_URL;
